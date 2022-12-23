8a57e17dbb226b29184c690754e179efc4825073
SMAILe3001 <smaile3001@gmail.com>
2022-12-24 00:30:51 +0200

    customer-reviews
    Створив файл slide.js з налаштуваннями слайдера
    Видалив невірні коментарі
    Додав імпорт налаштувань слайдера в index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,1 @@
-$('.slick-carousel').slick({
-  dots: true,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-});
+import "./js/slider.js";
---
+++ src/js/slider.js
@@ -0,0 +1,7 @@
+$('.slick-carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
